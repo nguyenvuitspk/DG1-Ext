d01abb7c5f94ea0692006d65543888ae65cdb200
TMA\vvphong <vovanphong15sk@gmail.com>
2021-05-13 09:00:35 +0700

    Phong commit code GrapQl

---
+++ GrapQL-Group_Phong_Thien/GrapQL/GrapQL/GrapQL/GraphQLQueries/AppQuery.cs
@@ -0,0 +1,91 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using GrapQL.Contracts;
+using GrapQL.GrapQL.GraphQLType;
+using GrapQL.Model;
+using GrapQL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrapQL.GrapQL.GraphQLQueries
+{
+    public class AppQuery : ObjectGraphType
+    {
+        private readonly IOwnerRepository _ownerrepository;
+        private readonly IAccountRepository _accountRepository;
+        public AppQuery(IOwnerRepository ownerrepository,
+                        IAccountRepository accountRepository)
+        {
+            _ownerrepository = ownerrepository;
+            _accountRepository = accountRepository;
+
+            Field<ListGraphType<OwnerType>>(
+               "owners",
+               arguments: new QueryArguments(
+                new QueryArgument<NonNullGraphType<PaginationInputType>> { Name = "Pagination" }
+                ),
+               resolve: context =>
+               {
+                   var page = context.GetArgument<Pagination>("Pagination");
+                   return _ownerrepository.GetAll(page.PageIndex, page.PageSize);
+               }
+            );
+            Field<OwnerType>(
+              "owner",
+            arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "ownerId" }),
+            resolve: context =>
+            {
+                Guid id;
+                if (!Guid.TryParse(context.GetArgument<string>("ownerId"), out id))
+                {
+                    context.Errors.Add(new ExecutionError("Wrong value for guid"));
+                    return null;
+                }
+                return _ownerrepository.GetById(id);
+            }
+            );
+            Field<TotalRowType>(
+               "getTotalRowOwner",
+               resolve: context =>
+               {
+                   return _ownerrepository.GetTotalRow();
+               }
+            );
+            Field<TotalRowType>(
+             "getTotalRowAcount",
+             resolve: context =>
+             {
+                 return _accountRepository.GetTotalRow();
+             }
+          );
+            Field<ListGraphType<AccountType>>(
+               "accounts",
+                arguments: new QueryArguments(
+                new QueryArgument<NonNullGraphType<PaginationInputType>> { Name = "Pagination" }
+                ),
+               resolve: context =>
+               {
+                   var page = context.GetArgument<Pagination>("Pagination");
+                   return _accountRepository.GetAll(page.PageIndex, page.PageSize);
+               }
+            );
+            Field<AccountType>(
+              "account",
+           arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "accountId" }),
+           resolve: context =>
+           {
+               Guid id;
+               if (!Guid.TryParse(context.GetArgument<string>("accountId"), out id))
+               {
+                   context.Errors.Add(new ExecutionError("Wrong value for guid"));
+                   return null;
+               }
+               return _accountRepository.GetById(id);
+           }
+           );
+
+        }
+    }
+}
---
+++ GrapQL-Group_Phong_Thien/GrapQL/GrapQL/Repository/AccountRepository.cs
@@ -0,0 +1,59 @@
+﻿using GrapQL.Contracts;
+using GrapQL.Entities.Context;
+using GrapQL.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrapQL.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly ApplicationContext _context;
+        public AccountRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Account> GetAllAccountsPerOwner(Guid ownerId) => _context.Accounts
+            .Where(a => a.OwnerId.Equals(ownerId))
+            .ToList();
+        public async Task<ILookup<Guid, Account>> GetAccountsByOwnerIds(IEnumerable<Guid> ownerIds)
+        {
+            var accounts = await _context.Accounts.Where(a => ownerIds.Contains(a.OwnerId)).ToListAsync();
+            return accounts.ToLookup(x => x.OwnerId);
+        }
+        public Account GetById(Guid id) => _context.Accounts.SingleOrDefault(o => o.Id.Equals(id));
+        public IEnumerable<Account> GetAll(int pageIndex, int pageSize)
+        {
+            return _context.Accounts.FromSqlInterpolated($"spGetAllAccount {pageIndex}, {pageSize}").ToList();
+        }
+        public Account CreateAccount(Account account)
+        {
+            account.Id = Guid.NewGuid();
+            _context.Add(account);
+            _context.SaveChanges();
+            return account;
+        }
+        public Account UpdateAccount(Account dbAccount, Account account)
+        {
+            dbAccount.Description = account.Description;
+            dbAccount.Type = account.Type;
+            dbAccount.OwnerId = account.OwnerId;
+            _context.SaveChanges();
+            return dbAccount;
+        }
+        public void DeleteAccount(Account account)
+        {
+            _context.Remove(account);
+            _context.SaveChanges();
+        }
+        public TotalRow GetTotalRow()
+        {
+            var totalRow = new TotalRow();
+            totalRow.Total = _context.Accounts.Count();
+            return totalRow;
+        }
+    }
+}
